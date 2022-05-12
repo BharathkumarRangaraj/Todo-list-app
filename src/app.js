678bacdd6d7ceaf48325946a3f4a6599b6300c83
Bharath Kumar <rbharathkumar12@gmail.com>
2022-05-12 16:34:43 +0530

    undo
    Signed-off-by: Bharath Kumar <rbharathkumar12@gmail.com>

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+class Mywebsite extends React.Component {
+  render() {
+    return <h1>Welcomeeew</h1>;
+  }
+}
+
+export default Mywebsite;
---
+++ src/notfound.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+const notfound = () => <h1>not found</h1>;
+export default notfound;
